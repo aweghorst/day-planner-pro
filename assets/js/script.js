c10ed841396dad752d3185f4b057ea92b5313dd2
aw2389 <alex2@weghorst.com>
2021-04-25 03:09:09 -0500

    styled containers relative to current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
-//Get Variables
-
 //Display Current Date
+var dateDisplay = moment().format("dddd, MMMM, Do");
+$("#currentDay").text(dateDisplay);
 
-//Style According to Current Time
+
+//STYLEROW FUNCTION
+function styleRow(currentHour, hour, id) {
+  var hour = i;
+  //Used to create a variable to dynamically fill in the correct element id in the for loop.
+  var id = "#time" + i;
+  //current hour in 24hr format
+  var currentHour = moment().format('H');
+
+  //Conditionals to add classes
+  if (currentHour > hour) {
+    $(id).addClass('past');
+  } else if (currentHour < hour) {
+    $(id).addClass('future');
+  } else {
+    $(id).addClass('present');
+  }
+}
+
+//Loop starting with 9am and ending after 9 hours
+for (var i = 9; i < 18; i++) {
+  styleRow();
+}
 
 //Save Button Clicked
 
